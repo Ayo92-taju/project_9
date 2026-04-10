2cb78da57a13717c50cedbd850c15df26d97580d
Ayo92-taju <69106007+Ayo92-taju@users.noreply.github.com>
2026-04-10 17:50:48 +0100

    fix: correct sqlite3 reference and improve product retrieval in get_products function

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ orders = []
 
 def get_db():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite.Row
+    conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
@@ -21,14 +21,20 @@ def home():
 
 @app.route('/products', methods=['GET'])
 def get_products():
+    conn = get_db()
+    products = conn.execute('SELECT * FROM products').fetchall()
+    conn.commit()
+    conn.close()
     return render_template('products.html', products=products)
 
 @app.route('/products', methods=['POST'])
 def add_product():
     name = request.form['name']
     price = float(request.form['price'])
-    product = Product(name, price)
-    products.append(product)
+    conn = get.db()
+    conn.execute('INSERT INTP products (name, price) VALUES (?, ?)', (name, price))
+    conn.commit()
+    conn.close()
     return redirect(url_for('get_products'))
 
 @app.route('/products/delete/<int:product_id>', methods=['POST'])
